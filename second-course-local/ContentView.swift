a6741f0a510322288ec65781536888428ceca44c
Natalya Murygina <nataly-mna@yandex.ru>
2022-03-04 10:44:58 +0400

    Initial Commit

---
+++ second-course-local/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  second-course-local
+//
+//  Created by Natalya Murygina on 04.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ second-course-local/second_course_localApp.swift
@@ -0,0 +1,17 @@
+//
+//  second_course_localApp.swift
+//  second-course-local
+//
+//  Created by Natalya Murygina on 04.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct second_course_localApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
